f5f8dee6d500c7623963d44e7c3b931a3e0e39ec
MarmoW <115847012+MarmoW@users.noreply.github.com>
2022-10-16 04:00:25 -0300

    botao agora some

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 
+
 function selecionarComida(item) {
     const rango = document.querySelector(".prato .selecionado");
     if ( rango !== null){
     rango.classList.remove("selecionado");}
    
     item.classList.add("selecionado");
-    
 
+    let a = document.querySelectorAll('.selecionado').length;
+    let b1 = document.querySelector('.botaopedido1');
+    if (a == 3){
+    b1.classList.remove('display');
+    return;
+    }
+        
+    
 }
+
+
 function selecionarBebida(item) {
     const rango = document.querySelector(".bebida .selecionado");
     if ( rango !== null){
     rango.classList.remove("selecionado");}
    
     item.classList.add("selecionado");
+
+    let a = document.querySelectorAll('.selecionado').length;
+    let b1 = document.querySelector('.botaopedido1');
+    if (a == 3){
+    b1.classList.remove('display');
+    return;
+    }
+        
     
 
 }
@@ -21,8 +39,24 @@ function selecionarSobremesa(item) {
     const rango = document.querySelector(".sobremesa .selecionado");
     if ( rango !== null){
     rango.classList.remove("selecionado");}
-   
+
     item.classList.add("selecionado");
+
+    let a = document.querySelectorAll('.selecionado').length;
+    let b1 = document.querySelector('.botaopedido1');
+    if (a == 3){
+    b1.classList.remove('display');
+    return;
+    }
+        
     
+}
+
+
+function fecharpedido(){
+    alert("quase lá");
 
 }
+
+
+
